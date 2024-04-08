a08b37c72381e617d53b7e2cef077bfb0a194a12
Farseen <oss@itsfarseen.anonaddy.me>
2024-04-08 10:26:57 +0530

    Added tagged values, preserve size information
    Add tagged values to allow serializing conway.cddl.
    Needed to wrap variant of CBORItem in an object to do this.
    So went ahead and added size field as well.
    This will help to ensure that roundtrip doesn't change representation.

--- src/experiment-value-level/parser.test.ts
+++ src/experiment-value-level/parser.test.ts
@@ -1,30 +1,45 @@
 import { test, expect } from "@jest/globals";
-import { getBase, parse } from "./parser";
+import { CBORItem, CBORItem_, getBase, parse } from "./parser";
+
+function v<T extends CBORItem["type"], V extends CBORItem_<T>["value"]>(
+  type: T,
+  value: V,
+  size?: number
+): CBORItem_<T> {
+  return { type, size, value } as CBORItem_<T>;
+}
+
 const EXAMPLES = [
-	[new Map([["hello", "world"]]), "a16568656c6c6f65776f726c64"],
-	[[1n, 2n, "ASD"], "83010263415344"],
-	[false, "f4"],
-	[true, "f5"],
-	[null, "f6"],
-	[1.6, "fb3ff999999999999a"]
+  [
+    v("map", new Map([[v("tstr", "hello", 0), v("tstr", "world", 0)]]), 0),
+    "a16568656c6c6f65776f726c64",
+  ],
+  [
+    v("array", [v("uint", 1n, 0), v("uint", 2n, 0), v("tstr", "ASD", 0)], 0),
+    "83010263415344",
+  ],
+  [v("boolean", false), "f4"],
+  [v("boolean", true), "f5"],
+  [v("null", null), "f6"],
+  [v("float", 1.6, 8), "fb3ff999999999999a"],
 ] as const;
 
 test("examples", function() {
-	for (let [js, cborHex] of EXAMPLES) {
-		let bytes = Uint8Array.from(Buffer.from(cborHex, "hex"));
-		let [out, _] = parse(bytes);
-		expect(out).toEqual(js)
-	}
+  for (let [js, cborHex] of EXAMPLES) {
+    let bytes = Uint8Array.from(Buffer.from(cborHex, "hex"));
+    let [out, _] = parse(bytes);
+    expect(out).toEqual(js);
+  }
 });
 
 test("getBase", () => {
-	expect(getBase(0x00)).toEqual(0x00);
-	expect(getBase(0x05)).toEqual(0x00);
-	expect(getBase(0x10)).toEqual(0x00);
-	expect(getBase(0x1F)).toEqual(0x00);
+  expect(getBase(0x00)).toEqual(0x00);
+  expect(getBase(0x05)).toEqual(0x00);
+  expect(getBase(0x10)).toEqual(0x00);
+  expect(getBase(0x1f)).toEqual(0x00);
 
-	expect(getBase(0x20)).toEqual(0x20);
-	expect(getBase(0x25)).toEqual(0x20);
-	expect(getBase(0x30)).toEqual(0x20);
-	expect(getBase(0x3F)).toEqual(0x20);
+  expect(getBase(0x20)).toEqual(0x20);
+  expect(getBase(0x25)).toEqual(0x20);
+  expect(getBase(0x30)).toEqual(0x20);
+  expect(getBase(0x3f)).toEqual(0x20);
 });
--- src/experiment-value-level/parser.ts
+++ src/experiment-value-level/parser.ts
@@ -1,255 +1,276 @@
+type SizeBytes = 0 | 1 | 2 | 4 | 8;
 type CBORItem =
-	| bigint
-	| string
-	| Uint8Array
-	| CBORItem[]
-	| Map<CBORItem, CBORItem>
-	| boolean
-	| null
-	| undefined
-	| number;
+  | { type: "uint"; size: SizeBytes; value: bigint }
+  | { type: "nint"; size: SizeBytes; value: bigint }
+  | { type: "tstr"; size: SizeBytes | null; value: string }
+  | { type: "bstr"; size: SizeBytes | null; value: Uint8Array }
+  | { type: "array"; size: SizeBytes | null; value: CBORItem[] }
+  | { type: "map"; size: SizeBytes | null; value: Map<CBORItem, CBORItem> }
+  | { type: "boolean"; value: boolean }
+  | { type: "null"; value: null }
+  | { type: "undefined"; value: undefined }
+  | { type: "float"; size: SizeBytes; value: number }
+  | { type: "tagged"; tag: number; value: CBORItem };
+
+type CBORItem_<T> = CBORItem & { type: T }
 
 function parse(stream: Uint8Array): [CBORItem, Uint8Array] {
-	switch (stream[0] & 0xf0) {
-		case 0x00:
-		case 0x10:
-			return _parseUInt(stream);
-		case 0x20:
-		case 0x30:
-			return _parseNegInt(stream);
-		case 0x40:
-		case 0x50:
-			return _parseByteString(stream);
-		case 0x60:
-		case 0x70:
-			return _parseTextString(stream);
-		case 0x80:
-		case 0x90:
-			return _parseArray(stream);
-		case 0xa0:
-		case 0xb0:
-			return _parseMap(stream);
-		case 0xf0:
-			switch (stream[0] & 0x0f) {
-				case 0x04:
-					return [false, stream.slice(1)]
-				case 0x05:
-					return [true, stream.slice(1)]
-				case 0x06:
-					return [null, stream.slice(1)]
-				case 0x07:
-					return [undefined, stream.slice(1)]
-				case 0x09:
-				case 0x0a:
-				case 0x0b:
-					return _parseFloat(stream);
-			}
-		default:
-			throw "Unknown type byte: " + stream[0].toString(16);
-	}
+  switch (stream[0] & 0xf0) {
+    case 0x00:
+    case 0x10:
+      return _parseUInt(stream);
+    case 0x20:
+    case 0x30:
+      return _parseNegInt(stream);
+    case 0x40:
+    case 0x50:
+      return _parseByteString(stream);
+    case 0x60:
+    case 0x70:
+      return _parseTextString(stream);
+    case 0x80:
+    case 0x90:
+      return _parseArray(stream);
+    case 0xa0:
+    case 0xb0:
+      return _parseMap(stream);
+    case 0xf0:
+      switch (stream[0] & 0x0f) {
+        case 0x04:
+          return [{ "type": "boolean", "value": false }, stream.slice(1)];
+        case 0x05:
+          return [{ "type": "boolean", "value": true }, stream.slice(1)];
+        case 0x06:
+          return [{ "type": "null", "value": null }, stream.slice(1)];
+        case 0x07:
+          return [{ "type": "undefined", "value": undefined }, stream.slice(1)];
+        case 0x09:
+        case 0x0a:
+        case 0x0b:
+          return _parseFloat(stream);
+      }
+    default:
+      throw "Unknown type byte: " + stream[0].toString(16);
+  }
 }
 
-function _parseUInt(stream: Uint8Array): [bigint, Uint8Array] {
-	return _parseBigInt("UInt", stream);
+function _parseUInt(stream: Uint8Array): [CBORItem_<"uint">, Uint8Array] {
+  let [n, stream_] = _parseBigInt("uint", stream);
+  return [{ type: "uint", ...n }, stream_];
 }
 
-function _parseNegInt(stream: Uint8Array): [bigint, Uint8Array] {
-	let [n, stream_] = _parseBigInt("NegInt", stream);
-	return [-1n - n, stream_];
+function _parseNegInt(stream: Uint8Array): [CBORItem_<"nint">, Uint8Array] {
+  let [n, stream_] = _parseBigInt("nint", stream);
+  n.value = -1n - n.value;
+  return [{ type: "nint", ...n }, stream_];
 }
 
 function __parseByteString(
-	schema: string,
-	stream: Uint8Array
-): [Uint8Array, Uint8Array] {
-	let type = stream[0];
+  schema: CBORItem["type"],
+  stream: Uint8Array
+): [CBORItem_<"bstr">, Uint8Array] {
+  let type = stream[0];
 
-	let base = getBase(type);
-	let type_ = type - base;
+  let base = getBase(type);
+  let type_ = type - base;
 
-	if (!((type_ >= 0x00 && type_ <= 0x1b) || type_ == 0x1f)) {
-		error(schema, type);
-	}
+  if (!((type_ >= 0x00 && type_ <= 0x1b) || type_ == 0x1f)) {
+    error(schema, type);
+  }
 
-	if (type_ == 0x1f) {
-		stream = stream.slice(1);
+  if (type_ == 0x1f) {
+    stream = stream.slice(1);
 
-		let chunks: Uint8Array[] = [];
-		let chunk: Uint8Array;
-		while (stream[0] != 0xff) {
-			[chunk, stream] = __parseByteString(schema, stream);
-			chunks.push(chunk);
-		}
-		return [_mergeChunks(chunks), stream];
-	} else {
-		let n: bigint;
-		[n, stream] = _parseBigInt(schema, stream);
+    let chunks: Uint8Array[] = [];
+    let chunk: CBORItem & { type: "bstr" };
+    while (stream[0] != 0xff) {
+      [chunk, stream] = __parseByteString(schema, stream);
+      chunks.push(chunk.value);
+    }
+    return [{ type: "bstr", size: null, value: _mergeChunks(chunks) }, stream];
+  } else {
+    let n;
+    [n, stream] = _parseBigInt(schema, stream);
 
-		let n_ = Number(n);
-		let buffer = stream.slice(0, n_);
-		stream = stream.slice(n_);
+    let n_ = bigintToNum(n.value);
+    let buffer = stream.slice(0, n_);
+    stream = stream.slice(n_);
 
-		return [buffer, stream];
-	}
-}
-function _parseByteString(stream: Uint8Array): [Uint8Array, Uint8Array] {
-	return __parseByteString("ByteString", stream);
+    return [{ type: "bstr", size: n.size, value: buffer }, stream];
+  }
 }
 
-function _parseTextString(stream: Uint8Array): [string, Uint8Array] {
-	let bytes: Uint8Array;
-	[bytes, stream] = __parseByteString("TextString", stream);
-
-	let str = new TextDecoder().decode(bytes);
-	return [str, stream];
+function _parseByteString(stream: Uint8Array): [CBORItem_<"bstr">, Uint8Array] {
+  return __parseByteString("bstr", stream);
 }
 
-function _parseArray(stream: Uint8Array): [CBORItem[], Uint8Array] {
-	let type = stream[0];
+function _parseTextString(stream: Uint8Array): [CBORItem_<"tstr">, Uint8Array] {
+  let bytes;
+  [bytes, stream] = __parseByteString("tstr", stream);
 
-	let base = getBase(type);
-	let type_ = type - base;
-
-	if (!((type_ >= 0x00 && type_ <= 0x1b) || type_ == 0x1f)) {
-		error("Array", type);
-	}
-
-	let array: CBORItem[] = [];
-	let item: CBORItem;
-
-	if (type_ == 0x1f) {
-		stream = stream.slice(1);
-
-		while (stream[0] != 0xFF) {
-			[item, stream] = parse(stream);
-			array.push(item);
-		}
-	} else {
-		let n: bigint;
-		[n, stream] = _parseBigInt("Array", stream);
-
-		let n_ = Number(n);
-
-		for (let i = 0; i < n_; i++) {
-			[item, stream] = parse(stream);
-			array.push(item);
-		}
-	}
-
-	return [array, stream];
+  let str = new TextDecoder().decode(bytes.value);
+  return [{ type: "tstr", size: bytes.size, value: str }, stream];
 }
 
-function _parseMap(stream: Uint8Array): [Map<CBORItem, CBORItem>, Uint8Array] {
-	let type = stream[0];
+function _parseArray(stream: Uint8Array): [CBORItem_<"array">, Uint8Array] {
+  let type = stream[0];
 
-	let base = getBase(type);
-	let type_ = type - base;
+  let base = getBase(type);
+  let type_ = type - base;
 
-	if (!((type_ >= 0x00 && type_ <= 0x1b) || type_ == 0x1f)) {
-		error("Map(" + base.toString(16) + ")", type);
-	}
+  if (!((type_ >= 0x00 && type_ <= 0x1b) || type_ == 0x1f)) {
+    error("array", type);
+  }
 
-	let map: Map<CBORItem, CBORItem> = new Map();
-	let key: CBORItem;
-	let val: CBORItem;
-	if (type_ == 0x1f) {
-		stream = stream.slice(1);
+  let array: CBORItem[] = [];
+  let item: CBORItem;
+  let size: SizeBytes | null = null;
 
-		while (stream[0] != 0xFF) {
-			[key, stream] = parse(stream);
-			[val, stream] = parse(stream);
-			map.set(key, val);
-		}
+  if (type_ == 0x1f) {
+    stream = stream.slice(1);
 
-	} else {
+    while (stream[0] != 0xff) {
+      [item, stream] = parse(stream);
+      array.push(item);
+    }
+  } else {
+    let n;
+    [n, stream] = _parseBigInt("array", stream);
 
-		let n: bigint;
-		[n, stream] = _parseBigInt("Map", stream);
+    let n_ = bigintToNum(n.value);
+    size = n.size;
 
-		let n_ = Number(n);
+    for (let i = 0; i < n_; i++) {
+      [item, stream] = parse(stream);
+      array.push(item);
+    }
+  }
 
-		for (let i = 0; i < n_; i++) {
-			[key, stream] = parse(stream);
-			[val, stream] = parse(stream);
-			map.set(key, val);
-		}
-	}
-
-	return [map, stream];
+  return [{ type: "array", size, value: array }, stream];
 }
 
-function _parseFloat(stream: Uint8Array): [number, Uint8Array] {
-	let view = new DataView(stream.buffer);
-	switch (stream[0]) {
-		case 0xf9:
-			throw "Half-precision floats are unsupported";
-		case 0xfa:
-			return [view.getFloat32(1, false), stream.slice(5)]
-		case 0xfb:
-			return [view.getFloat64(1, false), stream.slice(9)]
-		default:
-			throw "Unreachable"
-	}
+function _parseMap(stream: Uint8Array): [CBORItem_<"map">, Uint8Array] {
+  let type = stream[0];
+
+  let base = getBase(type);
+  let type_ = type - base;
+
+  if (!((type_ >= 0x00 && type_ <= 0x1b) || type_ == 0x1f)) {
+    error("map", type);
+  }
+
+  let map: Map<CBORItem, CBORItem> = new Map();
+  let size: SizeBytes | null = null;
+  let key: CBORItem;
+  let val: CBORItem;
+
+  if (type_ == 0x1f) {
+    stream = stream.slice(1);
+
+    while (stream[0] != 0xff) {
+      [key, stream] = parse(stream);
+      [val, stream] = parse(stream);
+      map.set(key, val);
+    }
+  } else {
+    let n;
+    [n, stream] = _parseBigInt("map", stream);
+
+    let n_ = bigintToNum(n.value);
+    size = n.size;
+
+    for (let i = 0; i < n_; i++) {
+      [key, stream] = parse(stream);
+      [val, stream] = parse(stream);
+      map.set(key, val);
+    }
+  }
+
+  return [{ type: "map", size, value: map }, stream];
+}
+
+function _parseFloat(stream: Uint8Array): [CBORItem_<"float">, Uint8Array] {
+  let view = new DataView(stream.buffer);
+  switch (stream[0]) {
+    case 0xf9:
+      throw "Half-precision floats are unsupported";
+    case 0xfa:
+      return [{ type: "float", size: 4, value: view.getFloat32(1, false) }, stream.slice(5)];
+    case 0xfb:
+      return [{ type: "float", size: 8, value: view.getFloat64(1, false) }, stream.slice(9)];
+    default:
+      throw "Unreachable";
+  }
 }
 
 function _mergeChunks(chunks: Uint8Array[]): Uint8Array {
-	let length = 0;
-	chunks.forEach((item) => {
-		length += item.length;
-	});
+  let length = 0;
+  chunks.forEach((item) => {
+    length += item.length;
+  });
 
-	let merged = new Uint8Array(length);
+  let merged = new Uint8Array(length);
 
-	let offset = 0;
-	chunks.forEach((item) => {
-		merged.set(item, offset);
-		offset += item.length;
-	});
+  let offset = 0;
+  chunks.forEach((item) => {
+    merged.set(item, offset);
+    offset += item.length;
+  });
 
-	return merged;
+  return merged;
 }
 
 function _parseBigInt(
-	schema: string,
-	stream: Uint8Array
-): [bigint, Uint8Array] {
-	let type = stream[0];
-	stream = stream.slice(1);
+  schema: CBORItem["type"],
+  stream: Uint8Array
+): [{ size: SizeBytes; value: bigint }, Uint8Array] {
+  let type = stream[0];
+  stream = stream.slice(1);
 
-	let base = getBase(type);
-	let type_ = type - base;
+  let base = getBase(type);
+  let type_ = type - base;
 
-	if (!(type_ >= 0x00 && type_ <= 0x1b)) {
-		error(schema + "(" + base.toString(16) + ")", type);
-	}
+  if (!(type_ >= 0x00 && type_ <= 0x1b)) {
+    error(schema, type);
+  }
 
-	let nBytes = type_ >= 0x18 ? Math.pow(2, type_ - 0x18) : 0;
+  let nBytes = type_ >= 0x18 ? Math.pow(2, type_ - 0x18) : 0;
 
-	if (type_ <= 0x17) {
-		return [BigInt(type_), stream];
-	}
+  if (type_ <= 0x17) {
+    return [{ size: 0, value: BigInt(type_) }, stream];
+  }
 
-	return [readBigInt(nBytes, stream), stream.slice(nBytes)];
+  return [
+    {
+      size: nBytes as SizeBytes,
+      value: readBigInt(nBytes, stream),
+    },
+    stream.slice(nBytes),
+  ];
 }
 
 function readBigInt(nBytes: number, stream: Uint8Array): bigint {
-	let x = BigInt(0);
-	for (let i = 0; i < nBytes; i++) {
-		x = x << 8n;
-		x += BigInt(stream[i]);
-	}
-	return x;
+  let x = BigInt(0);
+  for (let i = 0; i < nBytes; i++) {
+    x = x << 8n;
+    x += BigInt(stream[i]);
+  }
+  return x;
 }
 
 function getBase(type: number): number {
-	let tag = type >> 4;
-	let tagRoundDown = (tag >> 1) << 1;
-	let base = tagRoundDown << 4;
-	return base;
+  let tag = type >> 4;
+  let tagRoundDown = (tag >> 1) << 1;
+  let base = tagRoundDown << 4;
+  return base;
 }
 
-function error(expected: string, received: number) {
-	throw `Expected ${expected}. Received: ${received.toString(16)}`;
+function error(expected: CBORItem["type"], received: number) {
+  throw `Expected ${expected}. Received: ${received.toString(16)}`;
 }
 
-export { parse, getBase };
+function bigintToNum(x: bigint): number {
+  return Number(x)
+}
+
+export { parse, getBase, CBORItem, CBORItem_ };
