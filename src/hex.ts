16c97fc724eb0d9eb92d900de4b8934928c6667a
Farseen <oss@itsfarseen.anonaddy.me>
2024-08-14 19:00:54 +0530

    lib: fix bug in hex lookup table for non-lowercase hex values

--- src/hex.ts
+++ src/hex.ts
@@ -2,7 +2,7 @@ const BYTE_TO_HEX_LUT = new Array(0x100);
 const HEX_TO_BYTE_LUT: { [key: string]: number } = {};
 
 for (let i = 0; i < 0x100; i++) {
-  let hex = i.toString(16).padStart(2, "0");
+  let hex = i.toString(16).padStart(2, "0").toLowerCase();
   BYTE_TO_HEX_LUT[i] = hex;
   HEX_TO_BYTE_LUT[hex] = i;
 }
@@ -15,7 +15,7 @@ export function hexToBytes(hex: string): Uint8Array {
   let array = new Uint8Array(Math.floor(hex.length / 2));
   let i = 0;
   for (; i < array.length; i += 1) {
-    let byte = HEX_TO_BYTE_LUT[hex.slice(i * 2, i * 2 + 2)];
+    let byte = HEX_TO_BYTE_LUT[hex.slice(i * 2, i * 2 + 2).toLowerCase()];
     if (byte == undefined) break;
     array[i] = byte;
   }
