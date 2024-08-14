b6890e0cc1f2bd0eb505f647b1b987d1c77a54a1
Farseen <oss@itsfarseen.anonaddy.me>
2024-08-14 20:21:27 +0530

    codegen: fix issues with record_fragment and record_fragment_wrapper

--- conway-cddl/codegen/generate/record_fragment_wrapper.ts
+++ conway-cddl/codegen/generate/record_fragment_wrapper.ts
@@ -20,15 +20,13 @@ export class GenRecordFragmentWrapper implements CodeGenerator {
   generate(customTypes: SchemaTable): string {
     return `
       export class ${this.name} {
-        static FRAGMENT_FIELDS_LEN: number = ${this.item.type}.FRAGMENT_FIELDS_LEN;
-
         ${genMembers([this.item], customTypes)}
         ${genConstructor([this.item], customTypes)}
         ${genAccessors([this.item], customTypes)}
         ${genCSL(this.name)}
 
-        static deserialize(reader: CBORReader, len: number | null): ${this.name} {
-          let ${this.item.name} = ${this.item.type}.deserialize(reader, len);
+        static deserialize(reader: CBORReader): ${this.name} {
+          let ${this.item.name} = ${this.item.type}.deserialize(reader);
           return new ${this.name}(${this.item.name}) 
         }
 
