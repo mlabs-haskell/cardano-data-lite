8a55d572a44fdb9b8c57948eae25ade064394c4a
rmgaray <ramiro.sgaray@gmail.com>
2024-10-01 14:42:57 -0300

    fix: redundant jest config files & generated grammar import

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": [
+      "ts-jest",
+      {
+        isolatedModules: true, // disable type checking
+        useESM: true,
+      },
+    ],
   },
 };
