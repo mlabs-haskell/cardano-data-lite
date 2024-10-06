2dd6a9e941b077fcc2880cca706dcc496f2f99fd
rmgaray <ramiro.sgaray@gmail.com>
2024-10-06 13:52:42 -0300

    added new report with failing methods

--- tests/test_types.ts
+++ tests/test_types.ts
@@ -1,7 +1,7 @@
 // Type for transaction obtained from the blockchain
 export type TransactionInfo = { hash: string, cbor: string };
 
-// Type for method signatures of a given class
+// Types for method signatures of a given class
 export type ClassInfo = { name: string, methods: Array<MethodInfo> }
 export type MethodInfo = { name: string, static: boolean, params: Array<ParamInfo>, returnType: SomeType }
 export type ParamInfo = { name: string, type: SomeType }
@@ -22,4 +22,31 @@ export type SomeType =
     tag: "object",
     attrMap: Map<string, SomeType>
   }
+
+// Type for exports that rename classes
 export type ClassRename = { originalName: string, newName: string };
+
+// Types for method comparisons
+export type MethodComparisonResult = "success" | MethodComparisonFailure
+
+export type MethodComparisonFailure =
+  {
+    "reason": MethodComparisonFailureReason
+    "msg": string
+  }
+
+export type MethodComparisonFailureReason = 
+  "method_names_dont_match"
+  | "return_types_dont_match"
+  | "static_qualifiers_dont_match"
+  | "number_of_parameters_doesnt_match"
+  | "parameter_types_dont_match"
+
+// Types for type comparison
+export type TypeComparisonResult = "success" | TypeComparisonFailure
+
+export type TypeComparisonFailure = 
+  {
+    "expected": SomeType
+    , "received": SomeType
+  }
